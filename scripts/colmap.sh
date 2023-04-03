c4ad5a3647e8eae782b861c349c6fcd0df0df0c6
Gustav Hanning <gu6225ha-s@student.lu.se>
2023-04-03 12:12:31 +0200

    Change COLMAP options to get more matches.

--- scripts/colmap.sh
+++ scripts/colmap.sh
@@ -10,7 +10,7 @@ DB_PATH=$OUTPUT_PATH/database.db
 mkdir -p $OUTPUT_PATH
 COLMAP="${COLMAP_PATH:-colmap}"
 CAMERA_MODEL="${CAMERA_MODEL:-PINHOLE}"
-$COLMAP feature_extractor --database_path $DB_PATH --image_path $IMAGE_PATH --ImageReader.camera_model $CAMERA_MODEL
-$COLMAP exhaustive_matcher --database_path $DB_PATH
-$COLMAP mapper --database_path $DB_PATH --image_path $IMAGE_PATH --output_path $OUTPUT_PATH
+$COLMAP feature_extractor --database_path $DB_PATH --image_path $IMAGE_PATH --ImageReader.camera_model $CAMERA_MODEL --SiftExtraction.max_image_size 5000 --SiftExtraction.estimate_affine_shape=true --SiftExtraction.domain_size_pooling=true
+$COLMAP exhaustive_matcher --database_path $DB_PATH --SiftMatching.guided_matching=true
+$COLMAP mapper --database_path $DB_PATH --image_path $IMAGE_PATH --output_path $OUTPUT_PATH --Mapper.tri_ignore_two_view_tracks=false
 $COLMAP model_converter --input_path $OUTPUT_PATH/0 --output_path $OUTPUT_PATH --output_type TXT
